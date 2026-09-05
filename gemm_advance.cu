b2318b7453fd1f3cc8d173dab8f2ba59b1a72c4e
wangzixu <1415584788@qq.com>
2026-09-05 16:49:57 +0800

    one dim thread tile

--- gemm_advance.cu
+++ gemm_advance.cu
@@ -44,6 +44,52 @@ __global__ void mGemmAdvanceLaunch(int M, int N, int K,
         B += BLOCK_SIZE * N;
     }
     C[threadIdx.y * N + threadIdx.x] = tmp;
+}
+__device__ void print(const char* s) {
+    printf("%s", s);
+}
+
+template<typename T, typename... Args>
+__device__ void print(const char* s, T value, Args... args) {
+    printf(s, value, args...);
+}
+template<int BM, int BN, int BK, int TM>
+__global__ void mOnedimThreadTileGemmLaunch(int M, int N, int K,
+    const float* A, const float *B, float *C)
+{
+    const uint cRow = blockIdx.y, cCol = blockIdx.x;
+    const uint tRow = threadIdx.y, tCol = threadIdx.x;
+    A += cRow * K * BM;
+    B += cCol * BN;
+    C += cRow * N * BM + cCol * BN;
+    __shared__ float sa[BM][BK], sb[BK][BN];
+    float tmp[TM] = {0};
+    for (int i = 0; i < K; i += BK) {
+        if (tRow < BM && tCol < BK)
+            sa[tRow][tCol] = A[tRow * K + tCol];
+        if (tRow < BK && tCol < BN) 
+            sb[tRow][tCol] = B[tRow * N + tCol];
+        __syncthreads();
+
+        for (int dotIdx = 0; dotIdx < BK; dotIdx++) {
+            for (int resIdx = 0; resIdx < TM; resIdx++) {
+                int row = tRow * TM + resIdx;
+                if (row < BM) {
+                    tmp[resIdx] += sa[row][dotIdx] * sb[dotIdx][tCol];
+                }
+            }
+        }       
+        __syncthreads();
+
+        A += BK;
+        B += N * BK;
+    }
+    for (int i = 0; i < TM; i++) {
+        int row = tRow * TM + i;
+        if (row < BM && tCol < BN) {
+            C[row * N + tCol] = tmp[i];
+        }
+    }
 
 }
 
@@ -62,14 +108,15 @@ public:
         cudaMemcpy(dc, hc.p.get(), M * N * sizeof(float), cudaMemcpyHostToDevice);
 
         dim3 blockSize(BLOCK_SIZE, BLOCK_SIZE);
-        dim3 gridSize((M+BLOCK_SIZE-1)/BLOCK_SIZE,(N+BLOCK_SIZE-1)/BLOCK_SIZE);
-        mGemmAdvanceLaunch<<<gridSize,blockSize>>>(M,N,K,da,db,dc);
+        dim3 gridSize((N+BLOCK_SIZE-1)/BLOCK_SIZE,(M+BLOCK_SIZE-1)/BLOCK_SIZE);
+        // mGemmAdvanceLaunch<<<gridSize,blockSize>>>(M,N,K,da,db,dc);
+        mOnedimThreadTileGemmLaunch<32,32,8,4><<<gridSize, blockSize>>>(M,N,K,da,db,dc);
         cudaDeviceSynchronize();
         cudaMemcpy(hc.p.get(), dc, M * N * sizeof(float), cudaMemcpyDeviceToHost);
         cout << "GemmAdvance:" << endl;
         cout << hc << endl;
         Tensor<float> truth(M, N);
-        truth.fill(3);
+        truth.fill(9);
         cout << "======= test:" << (hc == truth) << "========\n";
         float *_free[] = {da, db, dc};
         for(int i = 0; i < sizeof(_free) / sizeof(float*); i++)
