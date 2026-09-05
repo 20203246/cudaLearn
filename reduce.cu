605f3471f31c3cc7e6e4b7f7c6ac082de48511ba
wangzixu <1415584788@qq.com>
2026-09-05 20:11:11 +0800

    优化reduce

--- reduce.cu
+++ reduce.cu
@@ -42,10 +42,26 @@ __global__ void reduceGpuInterleavedLaunch(int N, float *A, float *out) {
     out[blockIdx.x] = sa[0];
 }
 
+template<int BUFFER_SIZE>
+__global__ void reduceGpuUltraLaunch(int N, float *A, float *out) {
+    __shared__ float sa[BUFFER_SIZE];
+    const uint idx = blockDim.x * blockIdx.x + threadIdx.x;
+    if (idx < N) sa[threadIdx.x] = A[idx];
+    else sa[threadIdx.x] = 0;
+    __syncthreads();
+    for(int i = blockDim.x / 2; i > 0; i >>= 1) {
+        if (threadIdx.x < i){
+            sa[threadIdx.x] += sa[threadIdx.x + i];
+        }
+        __syncthreads();
+    }
+    out[blockIdx.x] = sa[0];
+}
+
 class ReduceKernel: public MMatmul {
 public:
     void test() override {
-        int M = 300;
+        int M = 300000;
         Tensor<float> ha(M, 1, 2);
         constexpr int BUFFER_SIZE = 256;
         Tensor<float> hb((M+BUFFER_SIZE-1)/BUFFER_SIZE,1,0);
@@ -57,9 +73,10 @@ public:
         cudaMemcpy(db, hb.p.get(), hb.M * sizeof(float), cudaMemcpyHostToDevice);
         dim3 gridSize((M+BUFFER_SIZE-1)/BUFFER_SIZE);
         dim3 blockSize(BUFFER_SIZE);
-        for(int lp = 0; lp < 1; lp++){
-            // reduceGpuBaselineLaunch<BUFFER_SIZE><<<gridSize,blockSize>>>(M,da,db);
+        for(int lp = 0; lp < 1000; lp++){
+            reduceGpuBaselineLaunch<BUFFER_SIZE><<<gridSize,blockSize>>>(M,da,db);
             reduceGpuInterleavedLaunch<BUFFER_SIZE><<<gridSize,blockSize>>>(M,da,db);
+            reduceGpuUltraLaunch<BUFFER_SIZE><<<gridSize,blockSize>>>(M,da,db);
         }
         cudaDeviceSynchronize();
         cudaMemcpy(ha.p.get(), da, M * sizeof(float), cudaMemcpyDeviceToHost);
